d4aa1e7dea42c3c66b74994f1f08cddc720bf465
Ángel Maroco <angelmaroco@gmail.com>
2022-04-13 09:18:21 +0200

    chore: eks bump version v0.8.2

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name            = "cluster-${var.tags.project}-${var.tags.environment}"
-  cluster_version = "1.21"
+  cluster_version = "1.22"
   region          = data.aws_region.current.name
 }
 
